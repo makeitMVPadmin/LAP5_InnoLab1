a305c1bfda0fd59e9121057531db3b722b769283
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-09-30 14:06:48 -0700

    deleted: styled componets for EventCard Component
    delete: global style for styled components in index
    
    delete: styled component from package json

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyle";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <GlobalStyle />
     <App />
   </BrowserRouter>
 );
