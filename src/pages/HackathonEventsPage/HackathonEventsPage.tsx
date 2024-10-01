762bc975ac0b6c9539e9e6b238aa036bb96b8e64
Nikita Van <138067509+nikitaszvan@users.noreply.github.com>
2024-10-01 09:30:35 -0400

    corrected bem scss

--- src/pages/HackathonEventsPage/HackathonEventsPage.tsx
+++ src/pages/HackathonEventsPage/HackathonEventsPage.tsx
@@ -4,72 +4,21 @@ import { Link } from "react-router-dom";
 
 const HackathonEventsPage = () => {
 
-
-
-   // Default to All Events
-
-  // // FIREBASE DATA
-  // useEffect(() => {
-  //   // Fetch user's communities
-  //   const fetchUserCommunities = async () => {
-  //     try {
-  //       const currentUser = auth.currentUser;
-  //       if (currentUser) {
-  //         const uid = currentUser.uid;
-  //         const userDocRef = doc(collection(db, "Users"), uid);
-  //         const userDocSnapshot = await getDoc(userDocRef);
-
-  //         if (userDocSnapshot.exists()) {
-  //           const userData = userDocSnapshot.data();
-  //           const joinedIds = userData.CommunitiesJoined || [];
-  //           const managedIds = userData.CommunitiesManage || [];
-
-  //           setUserCommunitiesJoined(joinedIds);
-  //           setUserCommunitiesManaged(managedIds);
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching user communities:", error);
-  //     }
-  //   };
-
-  //   fetchUserCommunities();
-  // }, []);
-
-  // useEffect(() => {
-  //   setUserCommunitiesJoined(data.users[0].communitiesJoined);
-  // }, []);
-  // useEffect(() => {
-  //   setUserCommunitiesManaged(data.users[0].communitiesManaged);
-  // }, []);
-  // useEffect(() => {
-  //   setUserCommunities([...userCommunitiesJoined, ...userCommunitiesManaged]);
-  // }, []);
-
-  // useEffect(() => {
-  //   setUserCommunities([1]);
-  //   setUserCommunitiesJoined([1]);
-  //   setUserCommunitiesManaged([1]);
-  // }, []);
-
-
-  //  DATA.JSON
-
   return (
     <div className="event-page">
       <DashboardNavbar/>
       <div className="event-page__banner">
-        <Link to="/" className="event-page__banner__btn-navigate-back">← Back</Link>
-        <h1 className="event-page__banner__header">Hackathon Events</h1>
-        <p className="event-page__banner__sub-header">Explore all the hackathon events</p>
+        <Link to="/" className="event-page__btn-navigate-back">← Back</Link>
+        <h1 className="event-page__header">Hackathon Events</h1>
+        <p className="event-page__sub-header">Explore all the hackathon events</p>
       </div>
       <div className="event-page__btn">
-        <Link to="#" className="event-page__btn__my-events">My Events</Link>
-        <Link to="#" className="event-page__btn__create-hackathon">Create Hackathon</Link>
+        <Link to="#" className="event-page__my-events">My Events</Link>
+        <Link to="#" className="event-page__create-hackathon">Create Hackathon</Link>
       </div>
       <div className="event-page__container">
-        <div className="event-page__container__filters">FILTER CONTAINER</div>
-        <div className="event-page__container__event-cards">CARDS CONTAINER</div>
+        <div className="event-page__filters">FILTER CONTAINER</div>
+        <div className="event-page__event-cards">CARDS CONTAINER</div>
       </div>
     </div>
   );
