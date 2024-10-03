0c77bbe68b4e7e476b5dccb392a09d4b1f18c08f
Nikita Van <138067509+nikitaszvan@users.noreply.github.com>
2024-10-02 20:59:29 -0400

    Update GetHackathonEvents.tsx (#10)
    * Update GetHackathonEvents.tsx
    
    * Update HackathonEventsPage.tsx

--- src/Firebase/GetHackathonEvents.tsx
+++ src/Firebase/GetHackathonEvents.tsx
@@ -1,8 +1,36 @@
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "./FirebaseConfig";
 
-export const fetchHackathonEvents = async () => {
+type HackathonEvent = {
+  basicProjectSummary: string;
+  createdAt: string;
+  disciplines: string[];
+  email: string;
+  endTime: string;
+  firstName: string;
+  fullDetails: string[];
+  imageUrl: string;
+  judges: string[];
+  lastName: string;
+  meetingLink: string;
+  participantCount: number;
+  participantIds: string[];
+  skillLevel: string;
+  startTime: string;
+  themes: string[];
+  timeZone: string;
+  title: string;
+}
+
+export const fetchHackathonEvents = async (): Promise<Record<string, HackathonEvent>> => {
   const colRef = collection(db, "hackathonEvents");
   const querySnapshot = await getDocs(colRef);
-  return querySnapshot.docs.map((doc) => doc.data());
-}
+  
+  const events = querySnapshot.docs.reduce((acc, doc) => {
+      acc[doc.id] = doc.data() as HackathonEvent;
+      return acc;
+  }, {} as Record<string, HackathonEvent>);
+
+  return events;
+};
+
