a902fd50e301d5f6139d3bcc82e06e7bd3c00019
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-04 10:09:00 -0700

    fix: (EventCard) link only certain parts, update heart like functionality

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -4,9 +4,11 @@ import { v4 as uuidv4 } from 'uuid';
 import HeartIconActive from "../../assets/images/favorite-icon-active.svg"
 import HeartIconInactive from "../../assets/images/favorite-icon-inactive.png"
 import EventStatusTag from '../EventStatusTag/EventStatusTag';
+import { Link } from "react-router-dom";
 
 
 interface EventCardProps {
+    id: string;
     title: string;
     startTime: string;
     endTime: string;
@@ -16,7 +18,7 @@ interface EventCardProps {
     themes: string[];
 }
 
-const EventCard: React.FC<EventCardProps> = ({ title, startTime, endTime, timeZone, skillLevel, themes, imageUrl }) => {
+const EventCard: React.FC<EventCardProps> = ({ title, id, startTime, endTime, timeZone, skillLevel, themes, imageUrl }) => {
     const formattedDate = `${convertToTimeZone(startTime, timeZone)} ${timeZone}`;
     const [isFavorite, setIsFavorite] = useState(false);
 
@@ -25,27 +27,34 @@ const EventCard: React.FC<EventCardProps> = ({ title, startTime, endTime, timeZo
         name: theme,
     }));
 
-    const handleFavorite = () => {
+    const handleFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation();
         setIsFavorite(!isFavorite);
     }
 
-    const tagStyle = "flex items-center justify-center px-4 font-gilroy h-7 rounded-full"
-    return (
-        <article className="flex flex-col w-[20.3125rem] h-[34.5625rem]">
-            <section className="relative h-[18.4375rem] border-[3px] border-MVP-black rounded-[24.2px] bg-cover bg-center w-full" style={{ backgroundImage: `url(${imageUrl})` }}>
-                <div className="relative p-[1.0625rem_0.8125rem] h-full">
-                    <div className="absolute flex gap-2 flex-wrap top-3 right-2.5">
-                        <span className={`${tagStyle} bg-MVP-black text-white`}>Joined</span>
-                        <EventStatusTag startTime={startTime} endTime={endTime} />
-                    </div>
-                    <div className="absolute flex items-end justify-between flex-wrap bottom-[1.0625rem] w-[92%]">
+    const tagStyle = "flex items-center justify-center px-4 font-gilroy h-7 rounded-full";
 
-                        <span className={`${tagStyle} border-MVP-black bg-MVP-white text-MVP-black border-2	`}>{skillLevel}</span>
-                        <div className="flex items-center justify-center w-[2.8rem] h-[2.8rem] rounded-full" onClick={handleFavorite}>
-                            <img className="w-full h-full" src={isFavorite ? HeartIconActive : HeartIconInactive} alt="favorite icon" />
+    return (
+        <article className="flex flex-col w-full md:w-80 h-[34.5625rem] mb-4">
+            <section className="relative " >
+                <button
+                    className="z-10	absolute bottom-4 right-4 flex items-center justify-center w-[2.8rem] h-[2.8rem] rounded-full"
+                    onClick={handleFavorite}
+                    aria-label="Toggle Favorite"
+                >
+                    <img className="w-full h-full" src={isFavorite ? HeartIconActive : HeartIconInactive} alt="favorite icon" />
+                </button>
+                <Link to={`/join-event/${id}`}>
+                    <div className="relative h-[18.4375rem] border-[3px] border-MVP-black rounded-[24.2px] bg-cover bg-center w-full" style={{ backgroundImage: `url(${imageUrl})` }}>
+                        <div className="absolute flex gap-2 flex-wrap top-3 right-2.5">
+                            <span className={`${tagStyle} bg-MVP-black text-white`}>Joined</span>
+                            <EventStatusTag startTime={startTime} endTime={endTime} />
+                        </div>
+                        <div className="absolute flex items-end justify-between flex-wrap bottom-[1.0625rem] w-[92%] left-2">
+                            <span className={`${tagStyle} border-MVP-black bg-MVP-white text-MVP-black border-2`}>{skillLevel}</span>
                         </div>
                     </div>
-                </div>
+                </Link>
             </section>
             <section className="flex flex-col justify-between pt-4 gap-4 flex-1 font-poppins">
                 <div className="flex flex-col gap-4">
@@ -57,12 +66,18 @@ const EventCard: React.FC<EventCardProps> = ({ title, startTime, endTime, timeZo
                         ))}
                     </div>
                 </div>
-                <button type="button" className="flex items-center justify-center w-[12.625rem] h-[3.9375rem] bg-MVP-light-blue border-[3px] border-t-[3px] border-r-[5px] border-b-[5px] border-l-[3px] border-MVP-black rounded-[0.625rem] text-[1.6875rem] font-gilroy cursor-pointer" aria-label="View Event">
-                    <span>View Event</span>
-                </button>
+                <Link to={`/join-event/${id}`}>
+                    <button
+                        type="button"
+                        className="flex items-center justify-center w-[12.625rem] h-[3.9375rem] bg-MVP-light-blue border-[3px] border-t-[3px] border-r-[5px] border-b-[5px] border-l-[3px] border-MVP-black rounded-[0.625rem] text-[1.6875rem] font-gilroy cursor-pointer"
+                        aria-label="View Event"
+                    >
+                        <span>View Event</span>
+                    </button>
+                </Link>
             </section>
         </article>
-    )
+    );
 }
 
 export default EventCard;
