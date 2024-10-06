e367558436b3ff9cfb6c38cc4ce1300bf9795913
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-05 23:29:19 -0700

    add: non dynamic inputs and textarea with form buttons

--- src/pages/ProjectSubmissionPage/ProjectSubmissionPage2.tsx
+++ src/pages/ProjectSubmissionPage/ProjectSubmissionPage2.tsx
@@ -101,6 +101,107 @@ const ProjectSubmissionPage2 = () => {
                 </div>
                 <Form {...form} >
                     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                        {/* Team Name */}
+                        <CustomInput
+                            errors={errors?.teamName}
+                            form={form}
+                            register={register}
+                            inputName="teamName"
+                            name="Team Name"
+                            formValues={formValues.teamName}
+                            placeHolder="Enter team name"
+                            type="Input"
+                        />
+                        {/* Tech Stack */}
+                        <CustomInput
+                            errors={errors?.techStack}
+                            form={form}
+                            register={register}
+                            inputName="techStack"
+                            name="Tech Stack"
+                            formValues={formValues.techStack}
+                            placeHolder="Enter Tech Stack"
+                            type="Input"
+                        />
+
+
+                        {/* Design Tools */}
+                        <CustomInput
+                            errors={errors?.designTools}
+                            form={form}
+                            register={register}
+                            inputName="designTools"
+                            name="Design Tools"
+                            formValues={formValues.designTools}
+                            placeHolder="Enter Design Tools"
+                            type="Input"
+                        />
+                        {/* Problem Statement */}
+                        <CustomInput
+                            errors={errors?.problemStatement}
+                            form={form}
+                            register={register}
+                            inputName="problemStatement"
+                            name="Problem Statement"
+                            formValues={formValues.problemStatement}
+                            placeHolder="Describe the problem statement"
+                            type="Textarea"
+                        />
+                        {/* Project Challenge */}
+                        <CustomInput
+                            errors={errors?.projectChallenge}
+                            form={form}
+                            register={register}
+                            inputName="projectChallenge"
+                            name="Problem Challenge"
+                            formValues={formValues.projectChallenge}
+                            placeHolder="What were some challenges or constraints?"
+                            type="Textarea"
+                        />
+                        {/* Design Features */}
+                        <CustomInput
+                            errors={errors?.designFeatures}
+                            form={form}
+                            register={register}
+                            inputName="designFeatures"
+                            name="Design Features"
+                            formValues={formValues.designFeatures}
+                            placeHolder="What is impact from your solution?"
+                            type="Textarea"
+                        />
+
+                        {/* Design Impact */}
+                        <CustomInput
+                            errors={errors?.designImpact}
+                            form={form}
+                            register={register}
+                            inputName="designImpact"
+                            name="Design Impact"
+                            formValues={formValues.designImpact}
+                            placeHolder="What is impact from your solution?"
+                            type="Textarea"
+                        />
+                        {/* Next Steps */}
+                        <CustomInput
+                            errors={errors?.nextSteps}
+                            form={form}
+                            register={register}
+                            inputName="nextSteps"
+                            name="Next Steps"
+                            formValues={formValues.nextSteps}
+                            placeHolder="What are your next steps and future plans on your solution?"
+                            type="Textarea"
+                        />
+                        {/* Upload image */}
+                        <img className="w-1/2 pt-10" src={UploadBox} alt="upload icon" />
+                        <div className="flex justify-end gap-2 mt-5">
+                            <Button type="submit" variant="default" >
+                                Join
+                            </Button>
+                            <Button type="button" variant="secondary">
+                                Cancel
+                            </Button>
+                        </div>
                     </form>
                 </Form>
             </section>
