4046f258a545c0dd15152cb22e2ebd18e6f0f3b2
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-06 18:15:35 -0700

    add: max file size and accepted image types in order to upload imgae

--- src/components/ImageUploadZone/ImageUploadZone.tsx
+++ src/components/ImageUploadZone/ImageUploadZone.tsx
@@ -1,15 +1,29 @@
 import { useDropzone } from 'react-dropzone';
 import UploadBox from "../../assets/images/uploadBox.svg"
 import { useState, useEffect } from 'react';
+import { STYLES } from '../../constants/styles';
 
+const MAX_SIZE = 10 * 1024 * 1024; // 10MB
+const ACCEPTED_TYPES = ['image/jpeg', 'image/png', 'application/pdf', 'image/svg+xml'];
 
 const ImageUploadZone = ({ onFileChange }) => {
     const [preview, setPreview] = useState(null);
-    const labelStyle = "block text-sm font-bold mb-1 text-MVP-black"
+    const [errorMessage, setErrorMessage] = useState(null);
+
 
     const onDrop = (acceptedFiles) => {
         const file = acceptedFiles[0];
         if (file) {
+            if (file.size > MAX_SIZE) {
+                setErrorMessage("File size exceeds 10MB");
+                return;
+            }
+            if (!ACCEPTED_TYPES.includes(file.type)) {
+                setErrorMessage("File type not supported. Please upload JPG, PNG, PDF, or SVG files.");
+                return;
+            }
+
+            setErrorMessage(null); // Reset any previous error message
             onFileChange(file);
             const previewUrl = URL.createObjectURL(file);
             setPreview(previewUrl);
@@ -28,10 +42,11 @@ const ImageUploadZone = ({ onFileChange }) => {
     const { getRootProps, getInputProps } = useDropzone({ onDrop });
     return (
         <div {...getRootProps()} className="dropzone">
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
             <input {...getInputProps()} />
             {preview ? (
                 <div className="mt-4">
-                    <h3 className={labelStyle}>Upload Project Files </h3>
+                    <h3 className={STYLES.label}>Upload Project Files </h3>
                     <img src={preview} alt="File Preview" className="max-w-xs border rounded-[10px]" />
                 </div>
             ) : <img className="pt-10" src={UploadBox} alt="upload icon" />
