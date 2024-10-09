d58ee0121ebca8db7c55e0077f5143e061a014b4
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-09 11:25:30 -0700

    add: EventPage bottom bar to link to projectSubmissionPage

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -1,10 +1,10 @@
-import DashboardNavbar from "@/components/DashboardNavbar/DashboardNavbar"
+import DashboardNavbar from "../../components/DashboardNavbar/DashboardNavbar"
 import { Link } from "react-router-dom"
 import Clock from "../../assets/images/clockIcon.svg"
 import { STYLES } from '../../constants/styles';
 
 const EventPage = () => {
-    return (<div className="w-full h-full bg-gradient-to-b from-MVP-extra-light-blue to-MVP-white bg-no-repeat">
+    return (<div className="w-full  relative bg-gradient-to-b from-MVP-extra-light-blue to-MVP-white bg-no-repeat">
         <DashboardNavbar />
         <div className="h-[22%] bg-MVP-light-gray flex flex-col justify-between px-8 py-8 max-h-[15rem] min-h-[12.5rem]">
             <Link to="/" className="text-MVP-black cursor-pointer">← Back</Link>
@@ -17,9 +17,12 @@ const EventPage = () => {
 
 
         </div>
-        <div className="h-[127px] absolute bg-MVP-white">
-            <div className="flex gap-2 "><img src={Clock} /><p>Time left to submit: 50m:15s</p></div>
-            <button className={STYLES.}></button>
+        <div className="h-[100px] w-full fixed bottom-0 px-8 border-t-[3px] border-MVP-black bg-MVP-white flex justify-between items-center">
+            <div className="flex gap-2 ">
+                <img className="h-6" src={Clock} />
+                <p className="font-gilroy">Time left to submit: 50m:15s</p>
+            </div>
+            <button className={`${STYLES.primaryButton} rounded-[10px] w-auto`}><Link to={`/event/0wPdqmuwFXyZawaf2Q9d/submit`} >Submit Project</Link></button>
         </div>
 
     </div>)
