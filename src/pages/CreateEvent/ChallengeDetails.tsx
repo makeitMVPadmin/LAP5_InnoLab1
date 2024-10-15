7b27c7e69728e5c828d8081d2293afd84b820ef3
Jiachun Xiang <jcxiang1230@gmail.com>
2024-10-15 00:01:29 -0400

    fix onSubmit function to save challenge data

--- src/pages/CreateEvent/ChallengeDetails.tsx
+++ src/pages/CreateEvent/ChallengeDetails.tsx
@@ -2,7 +2,7 @@ import { useForm, Controller } from "react-hook-form";
 import "./ChallengeDetails.scss";
 import { useNavigate } from "react-router-dom";
 import { CalendarIcon, ClockIcon } from "@heroicons/react/24/solid";
-// import { saveEventToFirestore } from "../../Firebase/Firebaseutils";
+import { saveEventToFirestore } from "../../Firebase/Firebaseutils";
 
 interface ChallengeDetailsFormInputs {
   challengeReleaseDate: string;
@@ -31,8 +31,11 @@ const ChallengeDetailsForm: React.FC = () => {
     },
   });
 
-  const onSubmit = (data: ChallengeDetailsFormInputs) => {
-    console.log(data);
+  const onSubmit = async (data: ChallengeDetailsFormInputs) => {
+    const combinedData = {
+      ...data,
+    };
+    await saveEventToFirestore(combinedData);
   };
   
 
