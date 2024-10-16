e13b945c9aeb7cfa65498a0f045a79265e2151eb
Jiachun Xiang <jcxiang1230@gmail.com>
2024-10-15 23:22:15 -0400

    remove duplicate for event page

--- src/pages/HackathonEventsPage/HackathonEventsPage.tsx
+++ src/pages/HackathonEventsPage/HackathonEventsPage.tsx
@@ -13,30 +13,27 @@ const HackathonEventsPage = () => {
   const { joinedEvents } = useJoinedEvents(currentUser?.uid);
   const { events, isLoading, getEndingEvent } = useEvents(joinedEvents);
   const { allCurrentEvents, joinedCurrentEvents } = events || {};
-  const [ alertEvent, setAlertEvent ] = useState(false);
+  const [alertEvent, setAlertEvent] = useState(false);
 
   useEffect(() => {
     setAlertEvent(getEndingEvent(joinedCurrentEvents));
   }, [joinedCurrentEvents]);
 
-
   const displayCards = useMemo(() => {
-    return (
-      allCurrentEvents.map(event => (
-        <EventCard
-          key={event.id}
-          id={event.id}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          startTime={event.startTime}
-          endTime={event.endTime}
-          timeZone={event.timeZone}
-          skillLevel={event.skillLevel}
-          themes={event.themes}
-          joined={joinedEvents.includes(event.id)}
-        />
-      ))
-    );
+    return allCurrentEvents.map((event) => (
+      <EventCard
+        key={event.id}
+        id={event.id}
+        imageUrl={event.imageUrl}
+        title={event.title}
+        startTime={event.startTime}
+        endTime={event.endTime}
+        timeZone={event.timeZone}
+        skillLevel={event.skillLevel}
+        themes={event.themes}
+        joined={joinedEvents.includes(event.id)}
+      />
+    ));
   }, [allCurrentEvents]);
 
   const renderEvents = () => {
@@ -48,48 +45,45 @@ const HackathonEventsPage = () => {
     <main className="w-full h-full bg-gradient-to-b from-MVP-extra-light-blue to-MVP-white bg-no-repeat">
       <DashboardNavbar />
       <div className="h-[22%] bg-MVP-light-gray flex flex-col justify-between px-8 py-8 max-h-[15rem] min-h-[12.5rem]">
-        <Link to="/" className="text-MVP-black cursor-pointer">← Back</Link>
+        <Link to="/" className="text-MVP-black cursor-pointer">
+          ← Back
+        </Link>
         <div>
-          <h1 className="font-corben text-[2.4rem] leading-[114%] md:text-[3rem] lg:text-[3rem]">Hackathon Events</h1>
+          <h1 className="font-corben text-[2.4rem] leading-[114%] md:text-[3rem] lg:text-[3rem]">
+            Hackathon Events
+          </h1>
           <p className="leading-[2.5]">Explore all the hackathon events</p>
         </div>
-// Jerrick-sprint-2
         <div className="event-page__container">
           <div className="event-page__filters">FILTER CONTAINER</div>
-          <div className="event-page__event-cards">
-          {hackathonEvents.map((event, index) => (
-            <Link to={`/details/${event.id}`} key={index}>
-            <EventCard
-              imageUrl={event.imageUrl}
-              title={event.title}
-              startTime={event.startTime}
-              endTime={event.endTime}
-              timeZone={event.timeZone}
-              skillLevel={event.skillLevel}
-              themes={event.themes}
-            />
-          </Link>
-  
-))}
-            </div>
-// Sprint 3
+        </div>
       </div>
       <div className="w-full flex justify-end gap-6 px-8 py-4 text-xl">
-        <Link to="joined" className="flex relative gap-2 py-2 px-4 border-3 border-black rounded-md bg-MVP-green text-MVP-black font-gilroy">
-          <Sensors className="w-7 h-7"/>
+        <Link
+          to="joined"
+          className="flex relative gap-2 py-2 px-4 border-3 border-black rounded-md bg-MVP-green text-MVP-black font-gilroy"
+        >
+          <Sensors className="w-7 h-7" />
           My Events
-          {alertEvent &&
+          {alertEvent && (
             <span className="absolute right-[-0.8em] top-[-0.8em] bg-MVP-black text-white text-sm rounded-full w-7 h-7 flex items-center justify-center">
               1
             </span>
-          }
+          )}
         </Link>
-        <Link to="/EventForm" className="py-2 px-4 border-3 border-black rounded-[8px] bg-MVP-dark-blue text-MVP-white font-gilroy">Create Hackathon</Link>
+        <Link
+          to="/EventForm"
+          className="py-2 px-4 border-3 border-black rounded-[8px] bg-MVP-dark-blue text-MVP-white font-gilroy"
+        >
+          Create Hackathon
+        </Link>
       </div>
       <div className="w-full h-full flex gap-4 mt-4 px-8">
-        <div className="flex-1 border-3 border-black w-[20%]">FILTER CONTAINER</div>
+        <div className="flex-1 border-3 border-black w-[20%]">
+          FILTER CONTAINER
+        </div>
         <div className="flex flex-wrap gap-4 mx-4 w-[80%]">
-        {renderEvents()}
+          {renderEvents()}
         </div>
       </div>
     </main>
@@ -97,4 +91,3 @@ const HackathonEventsPage = () => {
 };
 
 export default HackathonEventsPage;
-
