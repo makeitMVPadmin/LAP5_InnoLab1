542d75cf5be7406dab0de6d25402d8ffa21337cd
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-17 22:28:49 -0700

    update: styles for inboxCard for spacing and padding

--- src/components/ImportCard/ImportCard.tsx
+++ src/components/ImportCard/ImportCard.tsx
@@ -7,8 +7,8 @@ const ImportCard = ({ fileName, handleDelete }) => {
             <button className="w-11 h-11 absolute -right-4 top-0" type="button" onClick={handleDelete}>
                 <img className="w-2 h-2" src={CloseIcon} alt="delete icon" />
             </button>
-            <img src={ImportIcon} alt="import icon" />
-            <span className="font-poppins break-all	max-w-24 line-clamp-2">{fileName}</span>
+            <img src={ImportIcon} alt="import icon" className="h-10 basis-2" />
+            <span className="font-poppins break-all	max-w-24 line-clamp-2 basis-6">{fileName}</span>
         </article>
     )
 }
