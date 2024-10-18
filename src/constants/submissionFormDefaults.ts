9415443244a554d81a0f3b24a898bc5a4d42dd75
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-17 22:18:22 -0700

    update: formSchema to add pdfFiles

--- src/constants/submissionFormDefaults.ts
+++ src/constants/submissionFormDefaults.ts
@@ -10,6 +10,7 @@ export const DEFAULT_FORM_VALUES: ProjectSubmissionFormValues = {
   designImpact: "",
   nextSteps: "",
   imageFiles: [],
+  pdfFiles: [],
   projectLinks: [{ url: "" }],
   teamMembers: [{ name: "", role: "Software developer" }],
   eventId: "",
