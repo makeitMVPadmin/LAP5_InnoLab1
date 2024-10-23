c7b9ebd8e077dd9cfc4696e9cd5003e990ab3846
Nikita Van <138067509+nikitaszvan@users.noreply.github.com>
2024-10-23 15:51:45 -0400

    fix package dependency issues, fix issues in HackathoonEventsPage that arose when merging from Sprint-4

--- src/pages/HackathonEventsPage/HackathonEventsPage.tsx
+++ src/pages/HackathonEventsPage/HackathonEventsPage.tsx
@@ -3,28 +3,26 @@ import { useEffect, useState } from "react";
 import { useJoinedEvents } from "../../Firebase/FirebaseQueries";
 import { ReactComponent as CalendarRewind } from "../../assets/images/ic_round-event-repeat.svg";
 import { useAuth } from "../../context/AuthContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useEvents from "../../hooks/useEvents";
 import useFilterEvents from "../../hooks/useFilterEvents";
 import Filters from "../../components/Filters/Filters";
+
 
 const HackathonEventsPage = () => {
   const { currentUser } = useAuth();
   const { joinedEvents } = useJoinedEvents(currentUser?.uid);
   const { events, isLoading, getEndingEvent } = useEvents(joinedEvents);
   const { allCurrentEvents = [], joinedCurrentEvents = [] } = events || {};
-  const {
-    filters,
-    setFilters,
-    filteredEvents = [],
-  } = useFilterEvents(allCurrentEvents);
+  const { filters, setFilters, filteredEvents = [] } = useFilterEvents(allCurrentEvents);
   const [alertEvent, setAlertEvent] = useState(false);
 
   useEffect(() => {
     setAlertEvent(getEndingEvent(joinedCurrentEvents));
   }, [joinedCurrentEvents]);
 
-  const displayCards = filteredEvents?.map((event) => (
+
+  const displayCards = filteredEvents?.map(event => (
     <EventCard
       key={event.id}
       id={event.id}
@@ -42,8 +40,7 @@ const HackathonEventsPage = () => {
   const renderEvents = () => {
     if (isLoading) return <div>Loading...</div>;
 
-    if (!filteredEvents || filteredEvents.length === 0)
-      return <div>No Events</div>;
+    if (!filteredEvents || filteredEvents.length === 0) return <div>No Events</div>;
 
     return displayCards;
   };
@@ -54,25 +51,16 @@ const HackathonEventsPage = () => {
           <h1 className="font-corben text-[3.4rem] leading-[114%]">Events</h1>
           <p className="leading-[2.5 font-gilroy font-extrabold leading-[3rem]">Explore all the hackathon events</p>
         </div>
-        <div className="event-page__container">
-          <div className="event-page__filters">FILTER CONTAINER</div>
-        </div>
       </div>
       <div className="w-full flex justify-end gap-6 px-12 py-8 text-2xl font-gilroy font-extrabold">
         <Link to="joined" className="flex py-2.5 px-6 justify-center text-xl items-center gap-2.5 rounded-lg border-t-[0.2rem] border-r-[0.3rem] border-b-[0.3rem] border-l-[0.2rem] border-black bg-MVP-white font-gilroy font-extrabold">
           <CalendarRewind className="w-7 h-7" />
           My Events
-          {alertEvent && (
+          {alertEvent &&
             <span className="absolute right-[-0.8em] top-[-0.8em] bg-MVP-black text-white text-sm rounded-full w-7 h-7 flex items-center justify-center">
               1
             </span>
-          )}
-        </Link>
-        <Link
-          to="/EventForm"
-          className="py-2 px-4 border-3 border-black rounded-[8px] bg-MVP-dark-blue text-MVP-white font-gilroy"
-        >
-          Create Hackathon
+          }
         </Link>
         <Link to="/EventForm" className="flex py-2.5 px-6 justify-center text-xl items-center gap-2.5 rounded-lg border-t-[0.2rem] border-r-[0.3rem] border-b-[0.3rem] border-l-[0.2rem] border-black font-gilroy font-extrabold bg-MVP-dark-blue text-MVP-white">Create Hackathon</Link>
       </div>
