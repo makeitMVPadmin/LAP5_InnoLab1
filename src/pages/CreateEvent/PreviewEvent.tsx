1aeeba3a6d6091fadb0ca5d9c23f942973f94128
Jiachun Xiang <jcxiang1230@gmail.com>
2024-10-24 23:39:15 -0400

    chore: import embeded styles and add a section

--- src/pages/CreateEvent/PreviewEvent.tsx
+++ src/pages/CreateEvent/PreviewEvent.tsx
@@ -1,6 +1,9 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { saveEventToFirestore } from "../../Firebase/Firebaseutils";
 import { clearFormData } from "./StorageUtils";
+import { STYLES } from "../../constants/styles";
+
+const { styledBorder, sectionHeader } = STYLES;
 
 const PreviewEvent = () => {
   const navigate = useNavigate();
@@ -20,6 +23,32 @@ const PreviewEvent = () => {
 
   return (
     <div>
+              <div className="flex gap-[2rem] my-[3.5rem] font-extrabold text-[1.2rem]">
+          <div
+            className={`${styledBorder} rounded-none flex flex-1 gap-[0.8rem] !py-[0.6rem] !px-0 justify-center items-center text-center bg-MVP-yellow`}
+          >
+            <div className="flex justify-center items-center rounded-full h-full border-MVP-black border-[0.1rem] aspect-square">
+              1
+            </div>{" "}
+            Event Details
+          </div>
+          <div
+            className={`${styledBorder} rounded-none flex flex-1 gap-[0.8rem] !py-[0.6rem] !px-0 justify-center items-center text-center bg-MVP-yellow`}
+          >
+            <div className="flex justify-center items-center rounded-full h-full border-MVP-black border-[0.1rem] aspect-square">
+              2
+            </div>{" "}
+            Challenge Details
+          </div>
+          <div
+            className={`${styledBorder} rounded-none bg-MVP-yellow flex flex-1 gap-[0.8rem] !py-[0.6rem] !px-0 justify-center items-center text-center`}
+          >
+            <div className="flex justify-center items-center rounded-full h-full border-MVP-black border-[0.1rem] aspect-square">
+              3
+            </div>{" "}
+            Review
+          </div>
+        </div>
       <h1>Preview Your Event</h1>
       <div>
         <h2>{eventData.title}</h2>
